90984df8a0f4bd68f3552aec3f1208d029e9e812
wuhuqifei <1214086027@qq.com>
2023-12-12 23:14:16 +0800

    1

--- src/main/java/com/example/demo/Book.java
+++ src/main/java/com/example/demo/Book.java
@@ -2,17 +2,15 @@ package com.example.demo;
 
 import lombok.Data;
 
-/**
- * @author Administrator
- * @program: SpringBoot
- * @description:
- * @date 2023/12/7 23:04
- */
+
 @Data
 public class Book {
     private int id;
     private String name;
     private String type;
     private String description;
+
+
+
 
 }
