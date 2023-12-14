beef658533edeaa116dd648560595645071bdc89
wuhuqifei <1214086027@qq.com>
2023-12-15 00:31:27 +0800

    SpringBoot整合MongoDb

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/12/14 22:34
+ */
+@SpringBootApplication
+public class App {
+
+}
